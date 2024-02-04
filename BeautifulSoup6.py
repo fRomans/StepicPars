324bc8f6676d594b570a8b1b9e357c1203c76c8e
fRomanSt <vodaz@mail.ru>
2024-02-04 15:36:57 +0300

    4.61

--- BeautifulSoup6.py
+++ BeautifulSoup6.py
@@ -10,14 +10,15 @@ import requests
 # #Мы применили индексацию [-1], чтобы получить последний элемент списка, в котором хранился весь список значений пагинации.
 # print(pagen)
 
+
+
 # __________________________________________
 # Задача:Посетить указанный веб-сайт и извлечь названия товаров со всех четырех страниц.
 # Необходимо организовать данные таким образом, чтобы названия товаров с каждой страницы хранились в отдельном списке.
 # По завершении работы у вас должен быть главный список, содержащий четыре вложенных списка с названиями товаров.
 
-
 # Задаем URL-адрес веб-страницы для парсинга
-url = 'http://parsinger.ru/html/index1_page_3.html'
+url = 'http://parsinger.ru/html/index3_page_3.html'
 
 # Отправляем GET-запрос к указанной странице
 response = requests.get(url=url)
@@ -29,25 +30,30 @@ response.encoding = 'utf-8'
 soup = BeautifulSoup(response.text, 'lxml')
 
 # Ищем блок пагинации (элемент <div> с классом 'pagen') на странице,
-# затем извлекаем из него все вложенные ссылки (элементы <a>)
-pagen = soup.find('div', class_='pagen').find_all('a')
-
-
-
+# затем извлекаем (элементы <a>) и из него все вложенные ссылки
 pagen = [link['href'] for link in soup.find('div', class_='pagen').find_all('a')]
-
-# Выводим на экран список найденных ссылок
-print(pagen)
 
 # Инициализируем список для хранения абсолютных URL-адресов
 list_link = []
 
 # Задаем схему URL-адреса, которая будет использоваться для преобразования относительных путей в абсолютные URL
-schema = 'http://parsinger.ru/html/'
+schema = 'https://parsinger.ru/html/'
 
 # Цикл по всем найденным ссылкам для преобразования их в абсолютные URL-адреса
 for link in pagen:
     list_link.append(f"{schema}{link}")
 
-# Выводим на экран список абсолютных URL-адресов
-print(list_link)
+
+all_name = [] # тут все массивы страниц с наименованиями
+for li in list_link:
+    response = requests.get(url = li)
+    response.encoding = 'utf-8'
+    soup = BeautifulSoup(response.text, 'lxml')
+    tags = soup.findAll('a', class_='name_item')
+    this_name = [] #тут одна  страница с наименованиями
+    for tag in tags:
+            str1 = str(tag.text)
+            this_name.append(str1)
+    all_name.append(this_name)
+print(all_name)
+
