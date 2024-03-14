72e7e9732f22cc0beebbd1c5585b2b05bb94ce52
fRomanSt <vodaz@mail.ru>
2024-03-14 21:53:44 +0300

    1403

--- BeautifulSoup410.py
+++ BeautifulSoup410.py
@@ -82,52 +82,131 @@ import json
 # Сохраните данные в JSON файл с использованием указанных параметров.
 # json.dump(res, file, indent=4, ensure_ascii=False)
 
-url = 'https://parsinger.ru/html/index4_page_1.html'
-response = requests.get(url=url)
-response.encoding = 'utf-8'
-soup = BeautifulSoup(response.text, 'lxml')
+# url = 'https://parsinger.ru/html/index4_page_1.html'
+# response = requests.get(url=url)
+# response.encoding = 'utf-8'
+# soup = BeautifulSoup(response.text, 'lxml')
+#
+# pagen = [link['href'] for link in soup.find('div', class_='pagen').find_all('a')]
+#
+# schema = 'https://parsinger.ru/html/'
+#
+# list_link = []
+#
+# for link in pagen:
+#      list_link.append(f"{schema}{link}")
+#
+# nameAll = []
+# priceAll = []
+# descriptionAll = []
+# for stranica in list_link:
+#     response = requests.get(url=stranica)
+#     response.encoding = 'utf-8'
+#     soup = BeautifulSoup(response.text, 'lxml')
+#     for x in soup.find_all('a', class_='name_item'):
+#       nameAll.append(x.text.strip())
+#
+#     for x in soup.find_all('p', class_='price'):
+#         priceAll.append(x.text)
+#
+#     for x in soup.find_all('div', class_='description'):
+#         descriptionAll.append(x.text.strip().split('\n'))
+#
+# result_json = []
+# # 3 ------------------------------------------------------
+# for list_item, price_item, name in zip(descriptionAll, priceAll, nameAll):
+#
+#     result_json.append({
+#         'Наименование': name,
+#         'Бренд': [x.split(':')[1].strip() for x in list_item][0],
+#         'Форм-фактор': [x.split(':')[1].strip() for x in list_item][1],
+#         'Ёмкость': [x.split(':')[1].strip() for x in list_item][2],
+#         'Объем буферной памяти': [x.split(':')[1].strip() for x in list_item][3],
+#         'Цена': price_item
+#
+#     })
+#
+# with open('res.json', 'w', encoding='utf-8') as file:
+#         json.dump(result_json, file, indent=4, ensure_ascii=False)
 
-pagen = [link['href'] for link in soup.find('div', class_='pagen').find_all('a')]
 
-schema = 'https://parsinger.ru/html/'
+        # ____________________________________________________________________________________________________________________
+# Задача:Соберите данные всех карточек товара всех категорий и со всех страниц тренажера
+# (всего 160шт).
+# Не "проваливайтесь" внутрь каждой карточки. Соберите только информацию из превью.
+# Сохраните данные в JSON файл с использованием указанных параметров.
+# json.dump(res, file, indent=4, ensure_ascii=False).Порядок сбора категорий;
+# Часы
+# Телефоны
+# Мыши
+# HDD
+# Наушники
+# Имя файла произвольное.
+# Удалите все лишние пробелы из данных. методом .strip().
 
-list_link = []
-
-for link in pagen:
-     list_link.append(f"{schema}{link}")
+url1 = 'https://parsinger.ru/html/index1_page_1.html'
+url2 = 'https://parsinger.ru/html/index2_page_1.html'
+url3 = 'https://parsinger.ru/html/index3_page_1.html'
+url4 = 'https://parsinger.ru/html/index4_page_1.html'
+url5 = 'https://parsinger.ru/html/index5_page_1.html'
+urls = [url1,url2,url3,url4,url5]
 
 nameAll = []
 priceAll = []
 descriptionAll = []
-for stranica in list_link:
-    response = requests.get(url=stranica)
+res = []
+stranicaStr=""
+list_All_ref = []
+
+for url in urls:
+    # Отправляем GET-запрос к указанной странице
+    response = requests.get(url=url)
+
+    # Устанавливаем кодировку ответа сервера в UTF-8 для корректного отображения текста на кириллице
     response.encoding = 'utf-8'
+
+    # Преобразуем текст ответа сервера в объект BeautifulSoup с использованием парсера 'lxml'
     soup = BeautifulSoup(response.text, 'lxml')
-    for x in soup.find_all('a', class_='name_item'):
-      nameAll.append(x.text.strip())
 
-    for x in soup.find_all('p', class_='price'):
-        priceAll.append(x.text)
+    pagen = [link['href'] for link in soup.find('div', class_='pagen').find_all('a')]
+    schema = 'https://parsinger.ru/html/'
 
-    for x in soup.find_all('div', class_='description'):
-        descriptionAll.append(x.text.strip().split('\n'))
+    list_link = []
 
-result_json = []
-# 3 ------------------------------------------------------
+    for link in pagen:
+        list_link.append(f"{schema}{link}")
+
+
+    for stranica in list_link:
+
+        response = requests.get(url=stranica)
+        response.encoding = 'utf-8'
+        soup = BeautifulSoup(response.text, 'lxml')
+        for x in soup.find_all('a', class_='name_item'):
+            nameAll.append(x.text.strip())
+
+        for x in soup.find_all('p', class_='price'):
+            priceAll.append(x.text)
+
+        for x in soup.find_all('div', class_='description'):
+            descriptionAll.append(x.text.strip().split('\n'))
+
+
 for list_item, price_item, name in zip(descriptionAll, priceAll, nameAll):
+        
 
-    result_json.append({
-        'Наименование': name,
-        'Бренд': [x.split(':')[1].strip() for x in list_item][0],
-        'Форм-фактор': [x.split(':')[1].strip() for x in list_item][1],
-        'Ёмкость': [x.split(':')[1].strip() for x in list_item][2],
-        'Объем буферной памяти': [x.split(':')[1].strip() for x in list_item][3],
-        'Цена': price_item
+                    res.append({
+                        'Наименование': name,
+                        [x.split(':')[0].strip() for x in list_item][0] : [x.split(':')[1].strip() for x in list_item][0],
+                        [x.split(':')[0].strip() for x in list_item][1] : [x.split(':')[1].strip() for x in list_item][1],
+                        [x.split(':')[0].strip() for x in list_item][2] : [x.split(':')[1].strip() for x in list_item][2],
+                        [x.split(':')[0].strip() for x in list_item][3] : [x.split(':')[1].strip() for x in list_item][3],
+                        'Цена': price_item
 
-    })
+                             })
 
 with open('res.json', 'w', encoding='utf-8') as file:
-        json.dump(result_json, file, indent=4, ensure_ascii=False)
+    json.dump(res, file, indent=4, ensure_ascii=False)
 
 
 
