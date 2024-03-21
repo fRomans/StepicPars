700dd0d99e219f20e1a0f38834e451969fb182f0
fRomanSt <vodaz@mail.ru>
2024-03-21 18:22:49 +0300

    2103

--- BeautifulSoup411.py
+++ BeautifulSoup411.py
@@ -55,4 +55,98 @@ import requests
 # print(dict)
 
 # ____________________________________________________________________________________________________________________
-# Задача:
+# Задача:Сделать GET-запрос и получить JSON-структуру по указанной ссылке.
+# Для каждой категории товаров ('watch', 'mobile', 'mouse', 'hdd', 'headphones') рассчитать общую стоимость( Умножить стоимость товара на количество товара для каждой отдельной карточки).
+# Суммируйте значения для каждой отдельной категории.
+# Сформируйте словарь, где ключи - названия категорий, а значения - словаря с общей стоимостью товаров в этой категории.
+
+# url = 'https://parsinger.ru/downloads/get_json/res.json'
+#
+# response = requests.get(url=url).json()
+# watchPrice = 0
+# mobilePrice = 0
+# mousePrice = 0
+# hddPrice = 0
+# headphonesPrice = 0
+#
+# dict={}
+# for item in response:
+#     # print(item["categories"], item["count"])
+#     if(item["categories"]== "watch" ):
+#         price = int(str(item["price"]).replace(" руб", ""))
+#         count= int(item["count"])
+#         watchPrice += price*count
+#     elif(item["categories"]== "mobile" ):
+#         price = int(str(item["price"]).replace(" руб", ""))
+#         count= int(item["count"])
+#         mobilePrice += price*count
+#     elif(item["categories"]== "mouse" ):
+#         price = int(str(item["price"]).replace(" руб", ""))
+#         count= int(item["count"])
+#         mousePrice += price*count
+#     elif(item["categories"]== "hdd" ):
+#         price = int(str(item["price"]).replace(" руб", ""))
+#         count= int(item["count"])
+#         hddPrice += price*count
+#     elif(item["categories"]== "headphones" ):
+#         price = int(str(item["price"]).replace(" руб", ""))
+#         count= int(item["count"])
+#         headphonesPrice += price*count
+#
+# dict["watch"] = watchPrice
+# dict["mobile"] = mobilePrice
+# dict["mouse"] = mousePrice
+# dict["hdd"] = hddPrice
+# dict["headphones"] = headphonesPrice
+# print(dict)
+
+# ____________________________________________________________________________________________________________________
+# Задача:Получение данных: Используйте инструменты разработчика для определения источника данных(вкладка Network).
+# В нашем случае, данные лежат на этом веб-сайте.
+# Обработка данных: Извлеките данные со страницы и создайте словарь, в котором для каждой карточки вычислите произведение
+# значений "article" и "rating".
+# Сбор значений: Суммируйте результаты произведений для каждой категории.
+# Формирование словаря: Завершая задачу, создайте словарь, в котором ключами будут категории, а значениями - суммы произведений
+# "article" и "rating".
+
+url = 'https://parsinger.ru/4.6/1/res.json'
+
+response = requests.get(url=url).json()
+
+
+watchPr = 0
+mobilePr = 0
+mousePr = 0
+hddPr = 0
+headphonesPr = 0
+
+dict={}
+for item in response:
+
+    if(item["categories"]== "watch" ):
+        article = int(item["article"])
+        rating= int(item["description"]["rating"])
+        watchPr += article * rating
+    elif(item["categories"]== "mobile" ):
+        article = int(item["article"])
+        rating= int(item["description"]["rating"])
+        mobilePr += article * rating
+    elif(item["categories"]== "mouse" ):
+        article = int(item["article"])
+        rating= int(item["description"]["rating"])
+        mousePr += article * rating
+    elif(item["categories"]== "hdd" ):
+        article = int(item["article"])
+        rating= int(item["description"]["rating"])
+        hddPr += article * rating
+    elif(item["categories"]== "headphones" ):
+        article = int(item["article"])
+        rating= int(item["description"]["rating"])
+        headphonesPr += article * rating
+
+dict["watch"] = watchPr
+dict["mobile"] = mobilePr
+dict["mouse"] = mousePr
+dict["hdd"] = hddPr
+dict["headphones"] = headphonesPr
+print(dict)
