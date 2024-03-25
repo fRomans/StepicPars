342501acee5e8bd5f60f5635dee0e04816ef8230
fRomanSt <vodaz@mail.ru>
2024-03-25 15:22:38 +0300

    2503

---
+++ Selenium4.py
@@ -0,0 +1,12 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+# ____________________________________________________________________________________________________________________
+# Задача: поиском элемента и клика по нему.
+browser = webdriver.Chrome()
+browser.get('http://parsinger.ru/html/watch/1/1_1.html')
+button = browser.find_element(By.ID, "sale_button").click()
+
+time.sleep(10)
